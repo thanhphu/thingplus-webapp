96de1fb1fe693c2ec887dc1fe7847344762b5ac9
Thanh Phu <thanh.phu@daliworks.net>
2017-08-16 16:17:50 +0900

    Improve gateway UI

--- public/javascripts/gateways.js
+++ public/javascripts/gateways.js
@@ -1,10 +1,30 @@
+const interested = ['id','deviceModels','name','sensors','status']
+
 $.ajax({
     type: 'GET',
     url: '/gateways/forward',
     dataType: 'json',
     success: function (data) {
-        data.data.forEach(function (item) {
-            $('#gateways').append("<p><a href='#" + item.id + "'>" + item.name + "</a></p>");
-        });
+        $("tbody").empty();
+        var tbl_body = document.createElement("tbody");
+        var odd_even = false;
+
+        $.each(data.data, function () {
+            var tbl_row = tbl_body.insertRow();
+            tbl_row.className = odd_even ? "odd" : "even";
+            $.each(this, function (k, v) {
+                if (!$.inArray(k, interested)) {
+                    return;
+                }
+                var cell = tbl_row.insertCell();
+                var text = v.toString();
+                if (v instanceof Object) {
+                    text = JSON.stringify(v);
+                }
+                cell.appendChild(document.createTextNode(text));
+            })
+            odd_even = !odd_even;
+        })
+        $("#gateways-table").append(tbl_body);
     }
 });
--- public/javascripts/rules.js
+++ public/javascripts/rules.js
@@ -18,7 +18,6 @@ function loadRules() {
             $.each(this, function (k, v) {
                 var cell = tbl_row.insertCell();
                 var text = v.toString();
-                console.log(k);
                 if (v instanceof Object) {
                     text = JSON.stringify(v);
                 }
